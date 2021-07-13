7b58305f14c1fd9d96018b2751fbf9190b3e46ec
Aditi <aditiagarw4722@gmail.com>
2021-07-13 17:32:56 +0530

    task_18

---
+++ task__18/chess board/chessboard/src/components/block.jsx
@@ -0,0 +1,19 @@
+import Card1 from './card_black'
+import Card2 from './cards_white'
+const Board=()=>{
+    let list=[];
+    let count=0;
+    for(let i=0;i<8;i++)
+    {
+        for(let j=0;j<8;j++)
+        {
+            if(i%2===0)
+            list.push(<Card1 key={count}/>)
+            else
+            list.push(<Card2 key={count}/>)
+            count++;
+        }
+    }
+    return list;
+}
+export default Board;
